cd2ad7a9faa4be4aed968bc00ab2f0a329e69359
110318 <andrescamilogomezamaya@gmail.com>
2023-04-21 11:57:32 -0500

    Navbar Style pt1

--- public/Components/NavBar/navBar.js
+++ public/Components/NavBar/navBar.js
@@ -13,7 +13,7 @@ class NavBar extends HTMLElement {
         <link rel="stylesheet" href="../../../landing.css">
         
 
-        <nav class="navbar navbar-expand-lg navbar-light first-nav">
+        <nav class="navbar navbar-expand-lg navbar-light d-none  d-md-block first-nav">
             <div class="container-fluid">
 
                 <a class="navbar-brand" href="#">
@@ -24,18 +24,18 @@ class NavBar extends HTMLElement {
                 <form class="d-flex my-2 my-lg-0">
                     <input class="form-control me-2 placeholder-input-desktop headline5" type="search"
                     placeholder="¿Que estas buscando?" aria-label="Search">
-                <button class="btn search-button btn-active" type="submit"><i class="bi bi-search"></i></button>
+                <button class="btn btn_large search_button " type="submit"><i class="bi bi-search"></i></button>
                 </form>
             </div>
 
             <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item ">
-                        <a class="nav-link button-medium btn-active text-center text-button-link-normal" href="#"><i
+                        <a class="nav-link btn navup_buttons text-center text-button-link-normal" href="#"><i
                         class="bi bi-telephone distance-icon"></i>Canal de atención</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link button-medium btn-active text-center text-button-link-normal" style="margin-left: 10px;"
+                        <a class="nav-link  btn navup_buttons text-center text-button-link-normal" style="margin-left: 10px;"
                         href="#"><i class="bi bi-person distance-icon"></i>Mi perfil W</a>
                     </li>
                 </ul>
@@ -43,13 +43,13 @@ class NavBar extends HTMLElement {
         </div>
     </nav>
     
-        <nav class="navbar navbar-expand-lg navbar-dark  second-nav">
+        <nav class="navbar navbar-expand-lg navbar-dark  second_nav">
             <div class="container-fluid mx-auto ">
                 <a class="navbar-brand d-md-none" href="#">
                     <img class="logo" src="../images/logo-banco.png" alt="Logo de mi sitio web">
                 </a>
     
-            <button class="navbar-toggler ms-auto button-toggler" type="button" data-bs-toggle="collapse"
+            <button class="navbar-toggler ms-auto button_toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarNav2" aria-controls="navbarNav2" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="bi bi-list"></span>
             </button>
@@ -58,16 +58,16 @@ class NavBar extends HTMLElement {
                 <form id="search-2" class="d-md-none d-flex mx-auto my-2 my-lg-0">
                     <input class="form-control me-2 placeholder-input-mobile mx-auto" type="search"
                     placeholder="¿Que estas buscando?" aria-label="Search">
-                <button class="btn btn-outline search-button mx-auto" type="submit"><i class="bi bi-search"></i></button>
+                    <button class="btn btn_large search_button " type="submit"><i class="bi bi-search"></i></button>
                 </form>
 
-                <ul class="navbar-nav mx-auto">
+                <ul class="navbar-nav mx-auto second_nav_link">
                     <li class="nav-item d-md-none d-block">
-                        <a class="nav-link button-medium btn-active text-center text-white mx-auto" style="margin-top: 5px;"
+                        <a class="nav-link btn btn_large text-center text-white mx-auto" style="margin-top: 5px;"
                         href="#">Mi perfil W</a>
                     </li>
                     <li class="nav-item d-md-none d-block">
-                        <a class="nav-link button-medium btn-active text-center text-white mx-auto " style="margin-top: 18px;"
+                        <a class="nav-link btn btn_large text-center text-white mx-auto " style="margin-top: 18px;"
                         href="#">Canales de atención</a>
                     </li>
                     <li class="nav-item border-bottom-gg">
