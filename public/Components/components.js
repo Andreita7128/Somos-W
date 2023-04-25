3053778521301fdd89aa479c08910e1523d0ac33
110318 <andrescamilogomezamaya@gmail.com>
2023-04-25 11:28:35 -0500

    Credito semi finalizado

--- public/Components/components.js
+++ public/Components/components.js
@@ -1,5 +1,6 @@
 import "./NavBar/navBar.js"
 import "./Footer/footer.js"
+import "./Sidebarproduct/sidebarproduct.js"
 //import { cardBlogDetalle } from "./Cards/Card-article/cardArticle.js"
 //cardBlogDetalle
 import "./Footer/footer.js";
@@ -8,3 +9,4 @@ import "./tags/agenda.js"
 import "./tags/sidebar.js"
 import "./tags/breadcrumb.js"
 import "./tags/breadcomponent.js"
+import "./Cards/Card-article/cardArticle.js"
