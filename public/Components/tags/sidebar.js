c66b6ca2c19a981d7a7d222b7bbccaa59998dec6
jaeger2801 <juan.lopez48@u.icesi.edu.co>
2023-05-02 10:12:13 -0500

    Crea conexión entre páginas por el sidebar

--- public/Components/tags/sidebar.js
+++ public/Components/tags/sidebar.js
@@ -10,7 +10,7 @@ class Sidebar extends HTMLElement {
     render() {
         this.innerHTML = `
             <link rel="stylesheet" href="tags.css">
-            <div class="sidebar-container d-flex flex-column flex-shrink-0 p-3 bg-body-primary" >
+            <div class="sidebar-container d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" >
                 <h3 class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                     <svg class="bi pe-none me-2" width="40" height="32"></svg>
                     <span class="fs-4">Mi Perfil W</span>
@@ -19,7 +19,7 @@ class Sidebar extends HTMLElement {
                 <hr>
                 <ul class="nav nav-pills flex-column mb-auto">
                     <li class="side-item">
-                        <a href="#" class="nav-link side-link active " aria-current="page">
+                        <a href="/Perfil/perfil.html" class="nav-link side-link active " aria-current="page">
                         <svg class="bi pe-none me-2" width="16" height="16"></svg>
                         Perfil
                         </a>
@@ -33,14 +33,14 @@ class Sidebar extends HTMLElement {
                     </li>
 
                     <li class="side-item">
-                        <a href="#" class="side-link nav-link link-body-emphasis">
+                        <a href="/Perfil/giros.html" class="side-link nav-link link-body-emphasis">
                             <svg class="bi pe-none me-2" width="16" height="16"></svg>
                         Giros Internacionales
                         </a>
                     </li>
 
                     <li class="side-item">
-                        <a href="#" class="side-link nav-link link-body-emphasis">
+                        <a href="/Perfil/alcancias.html" class="side-link nav-link link-body-emphasis">
                         <svg class="bi pe-none me-2" width="16" height="16"></svg>
                         Alcancias
                         </a>
